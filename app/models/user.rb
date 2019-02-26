c09f1c8e42f506ab802e4493e79d2da649da5651
Eduardo Vilar <edu@novis.es>
2019-02-26 12:29:18 +0100

    Fixed confirmation mail from omniauth authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -85,8 +85,7 @@ class User < ActiveRecord::Base
       oauth_email: oauth_email,
       password: Devise.friendly_token[0, 20],
       terms_of_service: '1',
-      confirmed_at: oauth_email_confirmed ? DateTime.current : nil,
-      registering_with_oauth: true
+      confirmed_at: oauth_email_confirmed ? DateTime.current : nil
     )
   end
 
